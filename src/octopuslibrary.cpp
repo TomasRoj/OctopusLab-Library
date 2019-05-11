699b3bf745fc05a846d169305dae10787621559c
Tomáš Roj <rojtomas@email.cz>
2019-05-11 19:02:48 +0200

    More clear project structure + clean code

---
+++ src/octopuslibrary.cpp
@@ -0,0 +1,74 @@
+
+/* Include of the Arduino extension of cpp so we can
+ * use methods like pinmode() or digitalWrite().
+*/
+
+#include "Arduino.h"
+
+/* We include a .h file,
+ * which contains all the pins and functions what we use here.
+ * When using it we and our end-users can just call simple pin name
+ * and dont must worry about the pin number. The same principle
+ * we use in theese functions. The total purpose of this library
+ * in general isnt to do something like write something on display, 
+ * contact some server etc. It only does the job of initializing pins
+ * and make simpler and faster the development itself. 
+*/
+
+#include "octopuslibrary.h"
+
+// This function a test of built in led.
+void led_blinking() {
+
+  setupLed() // Calls function that sets pin mode etc. Dont forget that one of clean code rules is that
+	     //every function should do only one thing at the time. Thats the reason why there is call to function.
+
+  digitalWrite(BUILT_IN_LED, HIGH);
+  delay(1500);
+  digitalWrite(BUILT_IN_LED, LOW);
+  delay(1500);
+}
+
+//Sets up the LED
+void setupLed() {
+  pinMode(BUILT_IN_LED, OUTPUT);
+  digitalWrite(BUILT_IN_LED, LOW);
+}
+
+//Inicializes the motors.
+void setupMotors() {
+  //We dont need to use any 3. party lib if we can use digitalWrite() !
+  
+  pinMode(PIN_MOTOR_1A, OUTPUT);
+  pinMode(PIN_MOTOR_2A, OUTPUT);
+  pinMode(PIN_MOTOR_3A, OUTPUT);
+  pinMode(PIN_MOTOR_4A, OUTPUT);
+}
+
+void setupServo() {
+   //Include of the Arduino library for servo control.
+   #include <Servo.h>
+
+   Servo myservo;  // Creates servo object for the first servo to control a servo.
+   //Up to twelve servo objects can be created on most boards.
+
+   //We do some more for two more servo
+   Servo myservo_2;
+   Servo myservo_3;
+
+   //Now we attach every single servo to pin on the board.
+   myservo_1.attach(PIN_SERVO1);	
+   myservo_2.attach(PIN_SERVO2);
+   myservo_3.attach(PIN_SERVO3); 
+}
+
+
+/* This function setups all the components above.
+ * It gives user option to setup all in one function call.
+ * Thats good for clean code and more simple code in general.
+ */
+void setupAll() {
+  setupLed();
+  setupMotors();
+  setupServo();
+}
---
+++ src/octopuslibrary.h
@@ -0,0 +1,66 @@
+/*
+    octopuslibrary. Library to help users with using Octopuslab boards with ESP32: ROBOTboard and IOTboard
+    Originally created by Tomas Roj and ing. Jan Copak.
+    This and other files are available under MIT license. This comment must be included in any future distribution.
+*/
+
+
+/* This .h file contains all the pins and functions what will users use when imported. 
+ * It helps that we will not need to find original pin numbers and we can just remember
+ * the name what we want to use. While using library, user uses this file for the pin
+ * numbers.
+*/ 
+
+
+#define octopuslibrary_h
+
+/* We must include the official Arduino C++ extension so we can use
+ * methods like pinMode() or digitalWrite().
+*/
+
+#include "Arduino.h"
+
+// Below are the pins itself.
+
+//LED on board (built in)
+#define BUILT_IN_LED 2
+
+// All the pins used for DC motors
+#define PIN_MOTOR_12EN 25
+#define PIN_MOTOR_34EN 15
+#define PIN_MOTOR_1A 26
+#define PIN_MOTOR_2A 12
+#define PIN_MOTOR_3A 14
+#define PIN_MOTOR_4A 27
+
+//Buttons and other stuff
+#define PIN_WS 13   // RGB LED
+#define PIN_DEV1 32 // Dallas 1-Wire (thermometer)
+#define PIN_DEV2 33
+
+//Servo(s) / PWM
+#define PIN_SERVO1 17
+#define PIN_SERVO2 16
+#define PIN_SERVO3 4 // ROBOT board button
+
+//Analog inputs
+#define PIN_I36 36 //battery
+#define PIN_I39 39
+#define PIN_I34 34
+#define PIN_I35 35
+
+/* Here we define all the functions What we use in
+ * the .cpp file - the main code of the library.
+ * Any new function what we will want to implement later on
+ * we must define here first.
+*/ 
+
+// Pins for another things
+void led_blinking();
+void setupLed();
+void setupMotors();
+void setupServo();
+
+// This function setups all the above. Please edit this function too
+// so it can setup the new function too with others on one call.
+void setupAll()
